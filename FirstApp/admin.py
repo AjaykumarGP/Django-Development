41973cb8d3818e3ba418409fb6fef546c76463e1
Ajaykumar <ajaykumar11998@gmail.com>
2021-06-02 18:21:18 +0530

    Models and superuser created

--- FirstApp/admin.py
+++ FirstApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from FirstApp.models import Topic, WebPage, AccessRecord
+
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(WebPage)
+admin.site.register(Topic)
--- FirstApp/models.py
+++ FirstApp/models.py
@@ -9,3 +9,28 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Topic(models.Model):
+	
+	topicName = models.CharField(max_length=264, unique = True)
+
+	def __str__(self):
+		return self.topicName
+
+class WebPage(models.Model):
+	
+	topic = models.ForeignKey("Topic", on_delete=models.PROTECT)
+	name = models.CharField(max_length=264, unique=True)
+	url = models.URLField(unique=True)
+
+	def __str__(self):
+		return self.name
+
+class AccessRecord(models.Model):
+
+	name = models.ForeignKey("WebPage", on_delete=models.PROTECT)
+	date = models.DateField()
+
+	def __str__(self):
+		return str(self.date)
+		
